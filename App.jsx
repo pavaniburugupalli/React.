f9f64b13101a2749341ebbba80066be2c3071246
pavani burugupalli <147240686+pavaniburugupalli@users.noreply.github.com>
2024-09-24 10:37:49 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import helloworld from './HelloWorld'
+function App() {
+  return (
+    <div>
+      <h1>helloworld</h1>
+      
+    </div>
+  )
+}
+export default App
---
+++ HelloWorld.jsx
@@ -0,0 +1,10 @@
+import react from "react"
+function HelloWorld()
+{
+    return(
+        <div>
+            <h1>HelloWorld</h1>
+        </div>       
+    )
+}
+export default HelloWorld;
